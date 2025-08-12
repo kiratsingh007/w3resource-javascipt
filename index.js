869950c066f8a1ad0d2cad4db8193fd895df7cbd
Harkirat Singh <harkirat30.3.2006@gmail.com>
2025-08-12 08:31:08 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,76 @@
+/*let mode ="light";
+let color;
+ 
+if (mode === "dark") {
+    color="black";
+
+}
+else if(mode === "light"){
+    color= "blue";
+
+}
+else{
+    color="white";
+}
+ console.log(color); */
+
+ /*for loop
+  for( let i=1 ;i<=5 ;i++){
+
+    console.log("apna college");
+  }*/
+  /*while loop
+   let i=1;
+   while(i<=6) {
+    console.log("hello");
+    i++;
+   }*/
+  /* do-while loop
+   let i=1;
+   do{
+  console.log("hello");
+  i++;
+   }
+   while(i<=10);*/
+   /*_____________________
+    for of -loop: basically it was use for  strings.
+    let str="harkiratsingh";
+    let size=0;
+        for(let i of str){
+        console.log(i);
+        size++;
+    }
+    console.log(size);*/
+    /* _____________________
+    for in loop
+    let student={
+        name: "ram",
+        age: 17,
+        cgpa:8.9,
+
+ };
+    for(let i in  student){
+        console.log("key=",i,"value=",student[i]);
+    }
+*/
+/*for(let i=0;i<=100;i++){
+    if(i%2===0){
+        console.log("i=",i);
+    }
+
+}*/
+/*
+let gm= 35;
+let userno=prompt("guess the game number:");
+
+while (userno!= gm){
+ userno=prompt(" you entered the wrong number ,guess the game number:");
+}
+console.log("you guesss the right number");*/
+
+/*--------------------------------------------------------//
+let output=`now we are working on the template literal ${1+2+9}`;
+console.log(output);     
+*/
+
+   
